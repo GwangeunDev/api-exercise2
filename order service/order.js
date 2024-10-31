511ae0769c6b7b18945f1a19934a6276129aefa6
GwangEun <chlrhkddms77@gmail.com>
2024-10-31 22:17:43 +0900

    보상쿼리 구현

--- order service/order.js
+++ order service/order.js
@@ -18,7 +18,7 @@ db.serialize(() => {
 
 // (하드코딩된) 주문 정보
 let item_name = 'mango';
-let number_of_item = 5;
+let number_of_item = 50;
 const orderData = { item: item_name, number: number_of_item };
 
 // 주문 처리
@@ -36,31 +36,42 @@ app.get('/order', async (req, res) => {
 app.get('/insert', (req, res) => {
     const itemsToInsert = [
         { item: 'mango', number: 35 },
-        { item: 'apple', number: 50 },
-        { item: 'orange', number: 20 }
+        { item: 'pineapple', number: 50 },
+        { item: 'lemon', number: 20 }
     ];
 
     db.serialize(() => {
-        const insertStmt = db.prepare('INSERT INTO products (item, remaining_quantity) VALUES (?, ?)');
-
         itemsToInsert.forEach(({ item, number }) => {
-            insertStmt.run(item, number, (err) => {
+            // 먼저 아이템이 있는지 확인
+            db.get('SELECT remaining_quantity FROM products WHERE item = ?', [item], (err, row) => {
                 if (err) {
-                    console.error(`아이템 추가에 문제 생김... (${item})`, err);
+                    console.error(`아이템 조회 중 오류 발생 (${item})`, err);
+                    return res.status(500).send('아이템 조회 실패');
+                }
+
+                if (row) {
+                    // 아이템이 이미 있을 때: 수량 업데이트
+                    const updatedQuantity = row.remaining_quantity + number;
+                    db.run('UPDATE products SET remaining_quantity = ? WHERE item = ?', [updatedQuantity, item], (err) => {
+                        if (err) {
+                            console.error(`아이템 수량 업데이트 중 오류 발생 (${item})`, err);
+                        }
+                    });
+                } else {
+                    // 아이템이 없을 때: 새로 추가
+                    db.run('INSERT INTO products (item, remaining_quantity) VALUES (?, ?)', [item, number], (err) => {
+                        if (err) {
+                            console.error(`아이템 추가 중 오류 발생 (${item})`, err);
+                        }
+                    });
                 }
             });
         });
 
-        insertStmt.finalize((err) => {
-            if (err) {
-                console.error('마지막에 뭔가 잘못됐네요...', err);
-                res.status(500).send('추가 실패');
-            } else {
-                res.status(200).send('추가 성공했어요!');
-            }
-        });
+        res.status(200).send('재고 추가/업데이트 완료!');
     });
 });
+
 
 // 결과값 처리
 app.post('/update', (req, res) => {
@@ -70,6 +81,8 @@ app.post('/update', (req, res) => {
             res.status(500).send('물건을 가져오는데 문제가 있어요');
         } else if (!row) {
             res.status(404).send('물건을 찾을 수 없어요...');
+        } else if (row.remaining_quantity < number_of_item) {
+            res.status(400).send('주문하려는 수량이 남아 있는 개수보다 많아요...');
         } else {
             let remaining_quantity = row.remaining_quantity - number_of_item;
 
@@ -84,6 +97,7 @@ app.post('/update', (req, res) => {
         }
     });
 });
+
 
 // 모든 재고 내역 조회
 app.get('/products', (req, res) => {
